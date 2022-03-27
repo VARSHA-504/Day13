a07fa6ea9c0994b29aa787ddfd54694acf692099
varsha-504 <antargangivarsha.com>
2022-03-28 00:38:11 +0530

    added : generic class and generic type

--- Main.java
+++ Main.java
@@ -1,24 +1,38 @@
 package com.bl.generics;
 
-public class Main {
-    public static String maxString(String a, String b, String c) {
-        String max = a;// assume a is initially largest
-        if (b.compareTo(max) > 0)
-            max = b;
-        if (c.compareTo(max) > 0)
-            max = c;
 
-        System.out.println("First String= " + a);
-        System.out.println("Second String= " + b);
-        System.out.println("Third String= " + c);
-        System.out.println("Maximum Value is= " + max);
-        return max;
-    }
+    public class Main<T extends Comparable<T>> {
+        T a;
+        T b;
+        T c;
 
-    // main method
-    public static void main(String[] args) {
-        // Calling method with Integer argument
-        Main.maxString("Apple", "Peach", "Banana");
+        public Main(T a, T b, T c) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        @SafeVarargs
+        public static <T extends Comparable<T>> T testMaximum(T... value) {
+            T max = value[0]; // assume a is initially the largest
+
+            for (int i = 1; i < value.length; i++) {
+                if (value[i].compareTo(max) > 0) {
+                    max = value[i];
+                }
+            }
+            System.out.println("Maximum Value is= " + max);
+            return max; // returns the largest value
+        }
+
+
+        public static void main(String[] args) {
+
+            Main.<Integer>testMaximum(3, 4, 5, 9);
+
+            Main.<Float>testMaximum(6.6f, 8.8f, 7.7f, 9.3f);
+
+            Main.<String>testMaximum("Apple", "Pear", "Banana", "orange", "chiku");
 
     }
 }
