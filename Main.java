7464b48af71f422df7fa41d1875d951cda56d514
varsha-504 <antargangivarsha.com>
2022-03-28 00:26:02 +0530

    added:find max of 3 float values

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 package com.bl.generics;
 
 public class Main {
-    public static Integer maxInteger(Integer a, Integer b, Integer c) {
-        Integer max = a;// assume a is initially largest
+    public static Float maxFloat(Float a, Float b, Float c) {
+        Float max = a;// assume a is initially largest
         if (b.compareTo(max) > 0)
             max = b;
         if (c.compareTo(max) > 0)
@@ -10,14 +10,14 @@ public class Main {
 
         System.out.println("First Number= " + a);
         System.out.println("Sec Number= " + b);
-        System.out.println("Third Number= " + c);
+        System.out.println("Thrird Number= " + c);
         System.out.println("Maximum Value is= " + max);
         return max;
     }
 
-    // main method
+
     public static void main(String[] args) {
-        Main.maxInteger(7, 10, 9);
+        Main.maxFloat(7.3f, 10.1f, 9.0f);
 
     }
 }
