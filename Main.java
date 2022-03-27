0c9ecfb3a39ea68db315722c1417e8d3daf8b38f
varsha-504 <antargangivarsha.com>
2022-03-28 00:41:31 +0530

    added : generic class and generic type

--- Main.java
+++ Main.java
@@ -1,38 +1,23 @@
 package com.bl.generics;
 
+public class Main{
+    public static <T extends Comparable<T>> T maximum(T a, T b, T c) {
+        T max = a; // assume a is initially the largest
 
-    public class Main<T extends Comparable<T>> {
-        T a;
-        T b;
-        T c;
-
-        public Main(T a, T b, T c) {
-            this.a = a;
-            this.b = b;
-            this.c = c;
+        if (b.compareTo(max) > 0) {
+            max = b;
         }
-
-        @SafeVarargs
-        public static <T extends Comparable<T>> T testMaximum(T... value) {
-            T max = value[0]; // assume a is initially the largest
-
-            for (int i = 1; i < value.length; i++) {
-                if (value[i].compareTo(max) > 0) {
-                    max = value[i];
-                }
-            }
-            System.out.println("Maximum Value is= " + max);
-            return max; // returns the largest value
+        if (c.compareTo(max) > 0) {
+            max = c;
         }
+        return max; // returns the largest value
+    }
 
-
-        public static void main(String[] args) {
-
-            Main.<Integer>testMaximum(3, 4, 5, 9);
-
-            Main.<Float>testMaximum(6.6f, 8.8f, 7.7f, 9.3f);
-
-            Main.<String>testMaximum("Apple", "Pear", "Banana", "orange", "chiku");
-
+    // main method
+    public static void main(String[] args) {
+        // Calling method And passing values
+        System.out.printf("Max of %d, %d and %d is %d\n\n", 3, 4, 5, maximum(3, 4, 5));
+        System.out.printf("Max of %.1f,%.1f and %.1f is %.1f\n\n", 6.6, 8.8, 7.7, maximum(6.6f, 8.8f, 7.7f));
+        System.out.printf("Max of %s, %s and %s is %s\n", "Apple", "Pear", "Banana", maximum("Apple", "Pear", "Banana"));
     }
 }
