bbf6975273c14d0a2ae0f6c328548eae68863e52
varsha-504 <antargangivarsha.com>
2022-03-28 00:31:14 +0530

    added:find max of 3 Strings

--- Main.java
+++ Main.java
@@ -1,23 +1,24 @@
 package com.bl.generics;
 
 public class Main {
-    public static Float maxFloat(Float a, Float b, Float c) {
-        Float max = a;// assume a is initially largest
+    public static String maxString(String a, String b, String c) {
+        String max = a;// assume a is initially largest
         if (b.compareTo(max) > 0)
             max = b;
         if (c.compareTo(max) > 0)
             max = c;
 
-        System.out.println("First Number= " + a);
-        System.out.println("Sec Number= " + b);
-        System.out.println("Thrird Number= " + c);
+        System.out.println("First String= " + a);
+        System.out.println("Second String= " + b);
+        System.out.println("Third String= " + c);
         System.out.println("Maximum Value is= " + max);
         return max;
     }
 
-
+    // main method
     public static void main(String[] args) {
-        Main.maxFloat(7.3f, 10.1f, 9.0f);
+        // Calling method with Integer argument
+        Main.maxString("Apple", "Peach", "Banana");
 
     }
 }
