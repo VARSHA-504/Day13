cbca65b61939ea1779c2be749eb274944a472228
varsha-504 <antargangivarsha.com>
2022-03-28 00:23:39 +0530

    added:find max of 3 integers

--- Main.java
+++ Main.java
@@ -1,9 +1,23 @@
 package com.bl.generics;
 
 public class Main {
+    public static Integer maxInteger(Integer a, Integer b, Integer c) {
+        Integer max = a;// assume a is initially largest
+        if (b.compareTo(max) > 0)
+            max = b;
+        if (c.compareTo(max) > 0)
+            max = c;
 
+        System.out.println("First Number= " + a);
+        System.out.println("Sec Number= " + b);
+        System.out.println("Third Number= " + c);
+        System.out.println("Maximum Value is= " + max);
+        return max;
+    }
+
+    // main method
     public static void main(String[] args) {
-	    System.out.println("Welcome to generics program");
-        
+        Main.maxInteger(7, 10, 9);
+
     }
 }
